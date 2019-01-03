0dbb19199450aa8e65870cd5adb43678c2b1823d
gegy1000 <gegy1000@gmail.com>
2019-01-03 21:40:13 +0200

    Smoothed ocean edges

--- src/main/java/net/gegy1000/earth/server/world/pipeline/adapter/WaterCarveAdapter.java
+++ src/main/java/net/gegy1000/earth/server/world/pipeline/adapter/WaterCarveAdapter.java
@@ -7,6 +7,8 @@ import net.gegy1000.terrarium.server.world.pipeline.source.tile.ShortRasterTile;
 import net.gegy1000.terrarium.server.world.region.RegionData;
 
 public class WaterCarveAdapter implements RegionAdapter {
+    private static final int SMOOTH_RANGE = 5;
+
     private final RegionComponentType<WaterRasterTile> waterComponent;
     private final RegionComponentType<ShortRasterTile> heightComponent;
     private final int oceanDepth;
@@ -26,12 +28,43 @@ public class WaterCarveAdapter implements RegionAdapter {
             for (int localX = 0; localX < width; localX++) {
                 int waterType = waterTile.getWaterType(localX, localZ);
                 if (waterType != WaterRasterTile.LAND) {
-                    // TODO: Handle smooth edges
                     int waterLevel = waterTile.getWaterLevel(localX, localZ);
                     int depth = waterType == WaterRasterTile.OCEAN ? this.oceanDepth : Math.min(this.oceanDepth, 4);
-                    heightTile.setShort(localX, localZ, (short) (waterLevel - depth));
+                    double depthScale = this.computeDepthScale(waterTile, width, height, localZ, localX);
+
+                    double carvedHeight = waterLevel - depth * depthScale;
+                    heightTile.setShort(localX, localZ, (short) Math.round(carvedHeight));
                 }
             }
         }
+    }
+
+    private double computeDepthScale(WaterRasterTile waterTile, int width, int height, int x, int z) {
+        int landDistance = this.computeLandDistance(waterTile, width, height, z, x);
+        return (double) landDistance / SMOOTH_RANGE;
+    }
+
+    private int computeLandDistance(WaterRasterTile waterTile, int width, int height, int x, int z) {
+        for (int range = 0; range <= SMOOTH_RANGE; range++) {
+            int minX = x - range;
+            int maxX = x + range;
+            int minZ = z - range;
+            int maxZ = z + range;
+
+            for (int nz = minZ; nz <= maxZ; nz++) {
+                for (int nx = minX; nx <= maxX; nx++) {
+                    if (nx < 0 || nz < 0 || nx >= width || nz >= height) {
+                        continue;
+                    }
+                    if (nx == minX || nz == minZ || nx == maxX || nz == maxZ) {
+                        if (waterTile.getWaterType(nx, nz) == WaterRasterTile.LAND) {
+                            return range;
+                        }
+                    }
+                }
+            }
+        }
+
+        return SMOOTH_RANGE;
     }
 }
