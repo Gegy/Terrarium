42f1180117f82e37a11f89d6381241f2dfb04509
gegy1000 <gegy1000@gmail.com>
2019-03-30 13:24:47 +0200

    Bump version

--- src/main/java/net/gegy1000/earth/TerrariumEarth.java
+++ src/main/java/net/gegy1000/earth/TerrariumEarth.java
@@ -44,7 +44,7 @@ import java.util.Map;
 @Mod(modid = TerrariumEarth.MODID, name = "Terrarium: Earth", version = TerrariumEarth.VERSION, acceptedMinecraftVersions = "[1.12]", dependencies = "required-after:terrarium@[0.1.0,]")
 public class TerrariumEarth {
     public static final String MODID = "earth";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
 
     public static final String CLIENT_PROXY = "net.gegy1000.earth.client.ClientProxy";
     public static final String SERVER_PROXY = "net.gegy1000.earth.server.ServerProxy";
