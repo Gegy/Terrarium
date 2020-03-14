cedd93b836e42d98b754fe08f143ecc3104df261
gegy1000 <gegy1000@gmail.com>
2020-03-14 16:38:46 +0200

    better terrain colorization

--- src/main/java/net/gegy1000/earth/client/terrain/TerrainColorizer.java
+++ src/main/java/net/gegy1000/earth/client/terrain/TerrainColorizer.java
@@ -7,8 +7,10 @@ import net.gegy1000.earth.server.world.cover.CoverMarkers;
 import java.awt.Color;
 
 public final class TerrainColorizer {
+    private static final Color BARE_COLOR = new Color(160, 210, 112);
     private static final Color GRASS_COLOR = new Color(126, 208, 61);
-    private static final Color FOREST_COLOR = new Color(47, 111, 45);
+    private static final Color OPEN_FOREST_COLOR = new Color(53, 133, 49);
+    private static final Color CLOSED_FOREST_COLOR = new Color(47, 111, 45);
 
     private static final Color WATER_COLOR = new Color(51, 122, 255);
     private static final Color SLOPE_COLOR = Color.GRAY;
@@ -28,9 +30,16 @@ public final class TerrainColorizer {
         }
 
         if (cover.is(CoverMarkers.FOREST)) {
-            return FOREST_COLOR;
-        } else {
-            return GRASS_COLOR;
+            if (cover.is(CoverMarkers.CLOSED_FOREST)) {
+                return CLOSED_FOREST_COLOR;
+            }
+            return OPEN_FOREST_COLOR;
         }
+
+        if (cover.is(CoverMarkers.BARREN)) {
+            return BARE_COLOR;
+        }
+
+        return GRASS_COLOR;
     }
 }
